e40b799ab2db00489d6275dad3c43ca38274dbdb
DarkVex9 <notastonemason@gmail.com>
2025-11-29 14:00:26 -0600

    Initial CSS, more placeholder elements, rough javascript generation of code snippet divs

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Javascript Initialized");
+var resultsDiv = document.getElementById("results");
+
+init();
+
+
+function init(){
+  let sampleCode = "line 1\nline 2\n\nline4";
+  let sampleSource = "placeholder.js line 1";
+  resultsDiv.appendChild(createCodeSnippet(sampleCode,sampleSource));
+  console.log("Javascript Initialized");
+}
+
+function createCodeSnippet(code, labelText){
+    let div = document.createElement("div");
+    div.className = "box";
+    
+    let label = document.createElement("p");
+    label.appendChild(document.createTextNode(labelText));
+    div.appendChild(label);
+    
+    div.appendChild(document.createElement("br"));
+    
+    let codeBlock = document.createElement("div");
+    codeBlock.className = "codeBlock";
+    var splitCode = code.split("\n");
+    for (let i = 0; i < splitCode.length; i++){
+      let paragraph = document.createElement("p");
+      paragraph.appendChild(document.createTextNode(splitCode[i]));
+      codeBlock.appendChild(paragraph);
+    }
+    div.appendChild(codeBlock);
+    return div;
+}
